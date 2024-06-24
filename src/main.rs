d6be12cfec73a5812fec0b0cd050e10834cfb9d2
Ruslan Gorbunov <ruslangorbunovv@gmail.com>
2024-06-25 02:10:49 +0300

    fix prekol

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use rand::seq::SliceRandom;
 use rand::thread_rng;
 use rusb::{Context, DeviceHandle, Result, UsbContext};
 
-fn detach_device<T: UsbContext>(handle: &mut DeviceHandle<T>) -> Result<()> {
+fn detach_device<T: UsbContext>(handle: &DeviceHandle<T>) -> Result<()> {
     if let Ok(active) = handle.kernel_driver_active(0) {
         if active {
             handle.detach_kernel_driver(0)?;
@@ -19,17 +19,17 @@ async fn main() {
     loop {
         match scan_usb_devices() {
             Ok(handlers) => {
-                if let Some(mut random_handler) = handlers.choose(&mut thread_rng()) {
-                    match detach_device(&mut random_handler) {
+                if let Some(random_handler) = handlers.choose(&mut thread_rng()) {
+                    match detach_device(&random_handler) {
                         Ok(_) => println!("Device disconnected"),
-                        Err(e) => eprintln!("Failed disconnect device")
+                        Err(e) => eprintln!("Failed disconnect device: {}", e)
                     }
                 }
             },
             Err(e) => eprintln!("Error: {}", e),
         }
 
-        sleep(Duration::from_secs(10 * 60)).await.await;
+        sleep(Duration::from_secs(10 * 60)).await;
     }
 }
 
